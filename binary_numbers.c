5963e63bde64723b53891ed7c1d32cbfbd249b12
Your Name <you@example.com>
2022-04-21 15:32:24 -0700

    ddd

--- binary_numbers.c
+++ binary_numbers.c
@@ -9,35 +9,33 @@
 int use_binary(va_list arg)
 {
 /* variable declaration */
+	long int divisor = 2, len = 0, i, j, k, newval;
+	int *ptr;
+	unsigned int val = va_arg(arg, int);
 
-long int divisor = 2, len = 0, i, j, k, newval;
-int *ptr;
-unsigned int val = va_arg(arg, int);
-
-if (val == 0)
-_putchar ('0');
-len++;
-return (len);
-}
-newval = val;
-i = 0;
-while (newval > 0)
-{
-newval /= divisor;
-i++;
-}
-/* using malloc to create space for the length of numbers */
-ptr = malloc(sizeof(int) * i);
-if (ptr == NULL)
-return (-1);
-for (j = 0; j < i; j++)
-{
-ptr[j] = val % divisor;
-val /= divisor;
-}
-/* print the numbers in reverse by decrementing the value of k */
-for (k = j - 1; k >= 0; k--)
-_putchar(ptr[k] + '0');
-free(ptr);
-return (i);
+	if (val == 0)
+	{
+		_putchar ('0');
+		len++;
+		return (len);
+	}
+	newval = val;
+	i = 0;
+	while (newval > 0)
+	{
+		newval /= divisor;
+		i++;
+	}
+	ptr = malloc(sizeof(int) * i);
+	if (ptr == NULL)
+		return (-1);
+	for (j = 0; j < i; j++)
+	{
+		ptr[j] = val % divisor;
+		val /= divisor;
+	}
+	for (k = j - 1; k >= 0; k--)
+		_putchar(ptr[k] + '0');
+	free(ptr);
+	return (i);
 }
